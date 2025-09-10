25cd49e37ae9b00c6abed6197104b78ea2a0b1e1
FELIPE PEDROSO CANTANHEDE <felipe.cantanhede@etec.sp.gov.br>
2025-09-10 11:07:55 -0300

    cópia corrigida

--- src/Components/ProductForm.jsx
+++ src/Components/ProductForm.jsx
@@ -3,9 +3,9 @@ import { Form, Button } from "react-bootstrap";
 import api from "../services/api";
 import { useNavigate, useParams } from "react-router-dom";
 
-export default function Productform(){
+export default function ProductForm(){
     const{id} = useParams();
-    const navigate= useNavigate();
+    const navigate = useNavigate();
     const [product, setProduct] = useState({
         nome:"",
         preco:"",
@@ -51,11 +51,10 @@ export default function Productform(){
                     value={product.preco} onChange={handleChange} required/>
                  </Form.Group>
 
-                 <Form.Group className="mb-3">
-                    <Form.Label>Preço</Form.Label>
-                    <Form.Check type="checkbox" Label="Ativo" name="ativo"
-                        checked={product.ativo} onChange={handleChange} />
-                 </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Check type="checkbox" Label="Ativo" name="ativo"
+                                checked={product.ativo} onChange={handleChange} />
+                        </Form.Group>
 
                  <Button type="submit" variant="success">Salvar</Button>
          </Form>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'datatables.net-dt/css/dataTables.datatables.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
